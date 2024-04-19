e63f0c84d21833e22652bbce1d3c371ad6d9e39a
Przemek <93482731+przemekszw@users.noreply.github.com>
2024-04-19 23:17:25 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,23 @@
-var cacheName = 'my-site-cache-v1';
-var urlsToCache = [
+const staticDevCoffee = "dev-site-v1"
+const assets = [
   'index.html',
   'manifest.json',
   'icon.png'
 ];
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(urlsToCache);
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
     })
-  );
-  self.skipWaiting();
-});
+  )
+})
 
 /* Serve cached content when offline */
-self.addEventListener("fetch", function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
     })
-  );
-});
+  )
+})
